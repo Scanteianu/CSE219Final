7e77660b263e042f8e15e24dfe69697bbb66a774
Scanteianu <danielms853@gmail.com>
2015-12-04 23:40:48 -0500

    Select pages works, and updates dynamically

---
+++ src/cse219finalproj/DebugPrint.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cse219finalproj;
+
+/**
+ *
+ * @author Dan Scanteianu
+ */
+public class DebugPrint {
+    public static void println(String s)
+    {
+        System.out.println(s);
+    }
+    
+}
